587a4a5bb4db51ca7e6c9d7d78974fa878547126
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2020-05-23 01:38:31 +0700

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,1 +1,9 @@
+package com.company;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+
+public class Parser { //фабричный метод
+    HashMap<String,Commands> ComandsMap = new HashMap<String, Commands>(); //здесь храним название команды и ее алгоритм
+}
