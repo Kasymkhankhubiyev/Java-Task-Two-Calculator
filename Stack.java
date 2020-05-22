8cdf31b46562d830225e33c7fab893160e9cffe3
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2020-05-23 01:50:34 +0700

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,4 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Stack {
+    private ArrayList<Integer> stack = null; //основной стек, в котром будут храниться числа для взаимодействия
+    private ArrayList<figure> figures = null; //числа через DEFINE
+    private ArrayList<String> LineParam = null;
+    {
+        stack = new ArrayList<Integer>();
+        figures = new ArrayList<figure>();
+    }
+    public void AddNum(){
+        
+    }
 }
