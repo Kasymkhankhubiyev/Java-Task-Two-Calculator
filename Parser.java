3c68d039dc9f31b20505a6d1846a34df894aab43
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2020-05-23 09:34:43 +0700

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -3,7 +3,26 @@ package com.company;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+//должен быть фабричным
 
-public class Parser { //фабричный метод
-    HashMap<String,Commands> ComandsMap = new HashMap<String, Commands>(); //здесь храним название команды и ее алгоритм
+public class Parser {
+    HashMap<String, Commands> CommandsMap = new HashMap<String, Commands>(); //здесь храним название команды и ее алгоритм
+
+    public void process(Stack stack, String ComName) {
+        try {//фабричный метод
+            Commands command = CommandsMap.get(ComName);
+            if (command == null) {
+                Class<?> cl = Class.forName(ComName);
+                Constructor<?> constructor = cl.getDeclaredConstructor();
+                command = (Commands) constructor.newInstance();
+                CommandsMap.put(ComName, command);
+            }
+            command.ToDo(stack);
+        } catch (InvocationTargetException | IllegalAccessException
+                | ClassNotFoundException | NoSuchMethodException
+                | InstantiationException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
